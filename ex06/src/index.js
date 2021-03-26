c3d96f59a91d0e89df6659978a1ba632a9291b46
EdinCehajic <ed.cehajic@gmail.com>
2021-03-26 09:43:59 +0100

    Add

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,4 +1,10 @@
 // Only
 
+let rockStar = new Map ([["artist", "The Rolling Stones"],
+    ["song", "Angie"],
+    ["album", "Goats Head Soup"],
+    ["singer", "Mick Jagger"]]);
+
+
 // Only
 module.exports = { rockStar, newRock};
